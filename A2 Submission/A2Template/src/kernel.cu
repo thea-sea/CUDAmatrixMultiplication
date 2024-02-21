b1de19230a6db841091d43fb78cc6ffc4f800eb7
thea-sea <90663667+thea-sea@users.noreply.github.com>
2024-02-21 20:43:16 +0800

    2/10

--- A2 Submission/A2Template/src/kernel.cu
+++ A2 Submission/A2Template/src/kernel.cu
@@ -1,18 +1,3 @@
-/* Start Header *****************************************************************/
-
-/*! \file kernel.cu
-
-     \author Thea Sea, thea.sea, 2102348
-
-     \par email: thea.sea@digipen.edu
-
-     \date 19/2/2023
-
-     \brief Copyright (C) 2024 DigiPen Institute of Technology.
-
-  Reproduction or disclosure of this file or its contents without the prior written consent of DigiPen Institute of Technology is prohibited. */
-
-/* End Header *******************************************************************/
 /*
 * Copyright 2024 Digipen.  All rights reserved.
 *
@@ -25,21 +10,20 @@
 */
 #include <helper_cuda.h>
 #include "helper.h"
-//#include <cuda_runtime.h> //FIX __global__ and __shared__ undefined
+#include <crt/host_defines.h> //FIX __global__ and __shared__ undefined
 #include <device_launch_parameters.h> //fix blockIdx undefined
-
 
 //P and M column-major, N row-major
 __global__ void matrixMultiply(FLOAT_TYPE* P,       //<! [out] and mxn matrix
-	                            const FLOAT_TYPE* M, //<! [in] an mxk matrix
-	                            const FLOAT_TYPE* N, //<! [in] an kxn matrix
-	                            const int m, const int n, const int k) 
+    const FLOAT_TYPE* M, //<! [in] an mxk matrix
+    const FLOAT_TYPE* N, //<! [in] an kxn matrix
+    const int m, const int n, const int k)
 {
-	// Shared memory for tiling input N array
-	__shared__ FLOAT_TYPE N_s[TILE_WIDTH_RATIO_K][TILE_WIDTH_N];
+    // Shared memory for tiling input N array
+    __shared__ FLOAT_TYPE N_s[TILE_WIDTH_RATIO_K][TILE_WIDTH_N];
 
-	//do NOT change aboove
-	//your code here
+    //do NOT change aboove
+    //your code here
     int bx = blockIdx.x;
     int by = blockIdx.y;
     int tx = threadIdx.x;
@@ -56,7 +40,7 @@ __global__ void matrixMultiply(FLOAT_TYPE* P,       //<! [out] and mxn matrix
     }
 
     // Loop over the input tiles
-    for (int cnt = 0; cnt < (k - 1) / TILE_WIDTH_RATIO_K + 1; ++cnt) 
+    for (int cnt = 0; cnt < (k - 1) / TILE_WIDTH_RATIO_K + 1; ++cnt)
     {
         // Load the tile of N into shared memory
         int nIter = cnt * TILE_WIDTH_RATIO_K + tx;
@@ -94,22 +78,22 @@ __global__ void matrixMultiply(FLOAT_TYPE* P,       //<! [out] and mxn matrix
             P[Row * n + Col + j] = P_reg[j];
         }
     }
-	
+
 }
 
 void matrixMultiplyGPU(FLOAT_TYPE* P,
-	FLOAT_TYPE* M,
-	FLOAT_TYPE* N,
-	int numMRows,
-	int numNColumns,
-	int numMColumns)
+    FLOAT_TYPE* M,
+    FLOAT_TYPE* N,
+    int numMRows,
+    int numNColumns,
+    int numMColumns)
 {
-	//@@ Initialize the grid and block dimensions here
+    //@@ Initialize the grid and block dimensions here
 
-	dim3 dimGrid((numMRows - 1) / TILE_WIDTH_M + 1, (numNColumns - 1) / TILE_WIDTH_N + 1);
-	dim3 dimBlock(TILE_WIDTH_M, 1);
-	matrixMultiply<<<dimGrid, dimBlock>>>(P, M, N, numMRows, numNColumns, numMColumns);
+    dim3 dimGrid((numMRows - 1) / TILE_WIDTH_M + 1, (numNColumns - 1) / TILE_WIDTH_N + 1);
+    dim3 dimBlock(TILE_WIDTH_M, 1);
+    matrixMultiply << <dimGrid, dimBlock >> > (P, M, N, numMRows, numNColumns, numMColumns);
 
-	getLastCudaError("matrixMultiply failed\n");
-	cudaDeviceSynchronize();
+    getLastCudaError("matrixMultiply failed\n");
+    cudaDeviceSynchronize();
 }
